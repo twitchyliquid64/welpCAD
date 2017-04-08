78b97d860e0e6ba346333c0667e2b55d733bb0a1
Tom <twitchyliquid64@ciphersink.net>
2017-04-08 18:16:37 +1000

    Miniserv can now take a port to run on

--- serv.go
+++ serv.go
@@ -3,12 +3,17 @@ package main
 import (
   "log"
   "net/http"
+  "flag"
 )
 
+var listenAddr string
+
 func main() {
+  flag.StringVar(&listenAddr, "addr", ":3000", "Listener spec")
+  flag.Parse()
   fs := http.FileServer(http.Dir("static"))
   http.Handle("/", fs)
 
-  log.Println("Listening...")
-  http.ListenAndServe(":3000", nil)
+  log.Println("Listening on " + listenAddr + " ...")
+  http.ListenAndServe(listenAddr, nil)
 }
